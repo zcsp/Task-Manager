6e27635cad65c544bcf7a9250d5078f6619abdaa
spark33 <seanpark1107@gmail.com>
2022-06-19 01:58:55 -0400

    setting status colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,13 @@
 test_user = User.create(name: 'Sean')
 test_project = Project.create({ name: 'Brimmy'})
 task_group = TaskGroup.create({ name: 'Web', project_id: test_project.id })
-statuses = ['In Progress', 'Done', "Won't Be Done", 'Blocked']
-status = Status.create(statuses.map { |s| { name: s }})
+statuses = [
+  { name: 'In Progress', color: '#F3BF72' },
+  { name: 'Done', color: '#6AD096' },
+  { name: "Won't Be Done", color: '#7A7E91' },
+  { name: 'Stuck', color: '#D8707F' },
+]
+status = Status.create(statuses)
 tasks = Task.create({ name: 'Test', task_group_id: task_group.id })
 
 other_projects = ['WeScribe', 'Personal', "The Island's Box", 'EV Smart Island']
