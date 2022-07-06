c3541094093ab5a4d9afce56af31862302bb8669
spark33 <seanpark1107@gmail.com>
2022-07-06 02:56:20 -0400

    update order migration file

--- db/migrate/20220705234505_add_order_to_task_group.rb
+++ db/migrate/20220705234505_add_order_to_task_group.rb
@@ -1,5 +1,11 @@
 class AddOrderToTaskGroup < ActiveRecord::Migration[7.0]
   def change
     add_column :task_groups, :order, :integer
+
+    Project.find_each do |p|
+      p.task_groups.each do |tg, i|
+        tg.update_attribute(:order, i)
+      end
+    end
   end
 end
